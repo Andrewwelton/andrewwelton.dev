7cf7052fb5a30c53074f0746accb36cc348e351a
Andrewwelton <andrewwelton1337@gmail.com>
2019-04-23 21:37:11 -0400

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import "bulma/css/bulma.css";
+	export let name;
+</script>
+
+<style>
+	h1 {
+	  color: purple;
+	}
+</style>
+
+<section class="section">
+	<div class="container">
+		<h1 class="title">
+			Andrew Welton
+		</h1>
+		<h2 class="subtitle">Software Developer</h2>
+		<div class="columns">
+			<div class="column is-one-fifth">
+				<aside class="menu">
+					<p class="menu-label">
+						Things
+					</p>
+					<ul class="menu-list">
+						<li>
+							<a>About Me</a>
+						</li>
+					</ul>
+				</aside>
+			</div>
+		</div>
+	</div>
+</section>
